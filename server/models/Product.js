66df711745d65aec5e60c10ecbb4bfb36179de6b
Lev Sinitski <layatwar@gmail.com>
2025-12-08 20:34:34 +0200

    create Product model

---
+++ server/models/Product.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ['Main','Sushi']
+    },
+    description: {
+        type: String
+    },
+    isAvailable: {
+        type: Boolean,
+        default: true
+    }
+}, { timestamps: true });
+
+const Product = mongoose.model('Product', productSchema);
+export default Product;
