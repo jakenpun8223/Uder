34c684dbdfeabb57a8d53633fb9d1e43c7624c59
Lev Sinitski <layatwar@gmail.com>
2025-12-09 20:05:43 +0200

    create the table schema

---
+++ server/models/Table.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const tableSchema = new mongoose.Schema({
+    tableNumber: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    capacity: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['available', 'occupied', 'reserved'],
+        default: 'available'
+    },
+    needAssistance: {
+        type: Boolean,
+        default: false
+    },
+    currentOrder: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Order',
+        default: null
+    }
+}, { timestamps: true });
+
+export default mongoose.model('Table', tableSchema);
