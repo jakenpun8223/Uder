11ca3f3bee96f9861f9df1eef04d4e9b9b49d1c5
Lev Sinitski <layatwar@gmail.com>
2025-12-09 11:40:16 +0200

    -_-

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllUsers } from '../controllers/user.controller.js';
+import { getAllUsers } from '../controllers/userController.js';
 
 
 const router = express.Router();
