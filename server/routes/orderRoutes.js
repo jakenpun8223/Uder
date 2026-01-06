aed1920e58e0d3996fd7551b70111bf1b503fd9f
jakenpun8223 <byll647@gmail.com>
2026-01-06 11:04:13 +0200

    only roles may get or post things from the backend

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,20 +1,20 @@
 import express from 'express';
 import { createOrder, getAllOrders, updateOrderStatus, addItemsToOrder } from '../controllers/orderController.js';
-import { protect } from '../middleware/authMiddleware.js'; // Import protect
+import { protect, authorize } from '../middleware/authMiddleware.js'; // Import protect
 
 const router = express.Router();
 
 // WAITER ONLY: Start a new table order
 // Added 'protect' so unauthenticated customers cannot create orders
-router.post('/', protect, createOrder); 
+router.post('/', protect, authorize('staff', 'admin'), createOrder); 
 
 // WAITER: Add items
-router.post('/:id/add', protect, addItemsToOrder);
+router.post('/:id/add', protect, authorize('staff', 'admin'), addItemsToOrder);
 
 // KITCHEN: Get orders
-router.get('/', protect, getAllOrders);
+router.get('/', protect, authorize('kitchen', 'staff', 'admin'), getAllOrders);
 
 // CHEF: Update status
-router.patch('/:id/status', protect, updateOrderStatus);
+router.patch('/:id/status', protect, authorize('kitchen', 'staff', 'admin'), updateOrderStatus);
 
 export default router;
